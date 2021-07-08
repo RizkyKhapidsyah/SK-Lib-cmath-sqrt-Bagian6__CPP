9d9b0c44ed63f34f263501e6e985a37031514c84
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-08 21:30:50 +0700

    Add project files.

---
+++ SK-Lib-cmath-sqrt-Bagian6__CPP/Source.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    long long int val1 = 1000000000000000000;
+    long long int val2 = 999999999999999999;
+
+    cout << fixed << setprecision(12) << sqrt(val1) << endl;
+    cout << fixed << setprecision(12) << sqrt(val2) << endl;
+
+    _getch();
+    return (0);
+}
